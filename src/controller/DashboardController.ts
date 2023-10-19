aa71ba12b0bc0a7e848ed117c6f379d5bd6daf3e
Fernando Gurgel <gurgelff@gmail.com>
2023-10-19 17:16:42 -0300

    feat(controller): Dashboard com consumo de dados do JWT
    O email é obtido diretamente do JWT, o que permite posterior customização dinâmica do dashboard

---
+++ src/controller/DashboardController.ts
@@ -0,0 +1,74 @@
+// DashboardController.ts
+
+import jwt from 'jsonwebtoken';
+import { Request, Response } from 'express';
+import { User } from '../types/User';
+
+export class DashboardController {
+  /**
+   /**
+ * @swagger
+ * /dashboard:
+ *   get:
+ *     summary: Dashboard
+ *     tags: [Dashboard]
+ *     security:
+ *       - BearerAuth: []
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       '200':
+ *         description: 'Requisição executada com sucesso'
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "Olá, Mundo!"
+ *       '400':
+ *         description: 'Requisição inválida'
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: "Erro ao obter dados do usuário a partir do token."
+ */
+  public greet(req: Request, res: Response): Response {
+    try {
+      const [, token] = req.headers.authorization?.split(' ');
+      const user = jwt.verify(token, process.env.JWT_SECRET) as User;
+      const { email } = user;
+
+      return res.status(200).json({
+        status: true,
+        data: {
+          message: `Olá, ${email}!`
+        }
+      });
+    } catch (_err) {
+      return res.status(400).json({
+        status: false,
+        data: {
+          message: 'Erro ao obter dados do usuário a partir do token.'
+        }
+      });
+    }
+  }
+}
