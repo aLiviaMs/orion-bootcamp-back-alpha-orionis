31f867065f1043252f62df1aad167273a3a38e12
Fernando Gurgel <gurgelff@gmail.com>
2023-10-20 01:56:02 -0300

    refactor(routes): utiliza validateLogin como middleware para /login

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,11 @@ import { HomeController } from './controller/HomeController';
 import { AuthController } from './controller/AuthController';
 import { DashboardController } from './controller/DashboardController';
 import { jwtMiddleware } from './middleware/jwtMiddleware';
-
+import { validateLogin } from './middleware/validateLogin';
 const router = Router();
 
 router.get('/', new HomeController().hello);
-router.post('/login', new AuthController().login);
+router.post('/login', validateLogin, new AuthController().login);
 router.get('/dashboard', jwtMiddleware, new DashboardController().greet);
 
 export default router;
