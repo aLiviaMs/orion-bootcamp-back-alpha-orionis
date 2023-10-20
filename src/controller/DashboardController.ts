7121cad82c97a1bb280e946b6117a16bd33f44e6
Fernando Gurgel <gurgelff@gmail.com>
2023-10-20 00:57:52 -0300

    style(controller): formata comentários de Dashboard

--- src/controller/DashboardController.ts
+++ src/controller/DashboardController.ts
@@ -1,55 +1,52 @@
-// DashboardController.ts
-
 import jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
 import { User } from '../types/User';
 
 export class DashboardController {
   /**
-   /**
- * @swagger
- * /dashboard:
- *   get:
- *     summary: Dashboard
- *     tags: [Dashboard]
- *     security:
- *       - BearerAuth: []
- *     consumes:
- *       - application/json
- *     produces:
- *       - application/json
- *     responses:
- *       '200':
- *         description: 'Requisição executada com sucesso'
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 status:
- *                   type: boolean
- *                 data:
- *                   type: object
- *                   properties:
- *                     message:
- *                       type: string
- *                       example: "Olá, Mundo!"
- *       '400':
- *         description: 'Requisição inválida'
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 status:
- *                   type: boolean
- *                 data:
- *                   type: object
- *                   properties:
- *                     message:
- *                       type: string
- *                       example: "Erro ao obter dados do usuário a partir do token."
- */
+   * @swagger
+   * /dashboard:
+   *   get:
+   *     summary: Dashboard
+   *     tags: [Dashboard]
+   *     security:
+   *       - BearerAuth: []
+   *     consumes:
+   *       - application/json
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       '200':
+   *         description: 'Requisição executada com sucesso'
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: boolean
+   *                 data:
+   *                   type: object
+   *                   properties:
+   *                     message:
+   *                       type: string
+   *                       example: "Olá, Mundo!"
+   *       '400':
+   *         description: 'Requisição inválida'
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: boolean
+   *                 data:
+   *                   type: object
+   *                   properties:
+   *                     message:
+   *                       type: string
+   *                       example: "Erro ao obter dados do usuário a partir do token."
+   */
   public greet(req: Request, res: Response): Response {
     try {
       const [, token] = req.headers.authorization?.split(' ');
