4ac412b4520285cda4b2a1fa3bddf9d8c57bb751
Fernando Gurgel <gurgelff@gmail.com>
2023-10-20 01:55:01 -0300

    refactor(controller): remove lógica desnecessária/transportada de AuthController

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,28 +1,6 @@
-import { createJTW, comparePasswords } from '../utils/auth';
+import { createJTW } from '../utils/auth';
 import { Request, Response } from 'express';
-import { User, LoginRequestBody } from '../types/User';
-
-//@TODO: Remova - Classe de teste
-class UserDB {
-  static findOne({
-    where: { email }
-  }: {
-    where: { email: string };
-  }): Promise<User> {
-    return new Promise((resolve, reject) => {
-      if (email === 'email@domain.com') {
-        resolve({
-          id: 'bec36603-5f6b-4430-ba83-e5fe6a4d0989',
-          email: 'email@domain.com',
-          password:
-            '$2b$12$un44BpeTTyJQKrhf9K3xpuCKtyjvGQx2Aogt2QNkf0LZPgg3M9wdm' // 123456
-        });
-      }
-
-      reject({ erro: 'Usuário não encontrado.' });
-    });
-  }
-}
+import { ValidatedLoginRequestBody } from '../types/User';
 
 export class AuthController {
   /**
@@ -75,45 +53,14 @@ export class AuthController {
    */
 
   login = async (req: Request, res: Response): Promise<Response> => {
-    const { email, password, isRememberEnabled } = req.body as LoginRequestBody;
+    const { user, isRememberEnabled } = req.body as ValidatedLoginRequestBody;
+    const token: string = createJTW(user, isRememberEnabled);
 
-    if (!email || !password) {
-      return res.status(400).json({
-        status: false,
-        data: {
-          message: 'Email ou senha não informados.'
-        }
-      });
-    }
-
-    try {
-      const user: User = await UserDB.findOne({ where: { email } });
-      const isValidPassword = await comparePasswords(password, user.password);
-
-      if (!isValidPassword) {
-        return res.status(400).json({
-          status: false,
-          data: {
-            message: 'A senha digitada está incorreta. Tente novamente.'
-          }
-        });
+    return res.status(200).json({
+      status: true,
+      data: {
+        token
       }
-
-      const token: string = createJTW(user, isRememberEnabled);
-
-      return res.status(200).json({
-        status: true,
-        data: {
-          token
-        }
-      });
-    } catch (_err) {
-      return res.status(400).json({
-        status: false,
-        data: {
-          message: 'Não foi possível encontrar sua conta.'
-        }
-      });
-    }
+    });
   };
 }
