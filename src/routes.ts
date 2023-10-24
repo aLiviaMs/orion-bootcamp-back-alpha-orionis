4769e6966ab3eab53b611762d5c7865a711aae50
Fernando Gurgel <gurgelff@gmail.com>
2023-10-24 02:02:38 -0300

    feat(routes): usa ValidatePassword em /login como Middleware

--- src/routes.ts
+++ src/routes.ts
@@ -4,10 +4,16 @@ import { AuthController } from './controller/AuthController';
 import { DashboardController } from './controller/DashboardController';
 import { jwtMiddleware } from './middleware/jwtMiddleware';
 import { validateLogin } from './middleware/validateLogin';
+import { validatePassword } from './middleware/validatePassword';
 const router = Router();
 
 router.get('/', new HomeController().hello);
-router.post('/login', validateLogin, new AuthController().login);
+router.post(
+  '/login',
+  validatePassword,
+  validateLogin,
+  new AuthController().login
+);
 router.get('/dashboard', jwtMiddleware, new DashboardController().greet);
 
 export default router;
