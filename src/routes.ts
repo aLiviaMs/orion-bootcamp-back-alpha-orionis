04b39d37c45f5a0a483ea859e1b0ca344de96853
Hugo Tiburcio <hugotiburcio@outlook.com>
2023-10-26 15:00:19 -0300

    fix: remove unused route

--- src/routes.ts
+++ src/routes.ts
@@ -4,12 +4,10 @@ import { AuthController } from './controller/AuthController';
 import { DashboardController } from './controller/DashboardController';
 import { jwtMiddleware } from './middleware/jwtMiddleware';
 import { validateLogin } from './middleware/validateLogin';
-import { EmailController } from './controller/EmailController';
 const router = Router();
 
 router.get('/', new HomeController().hello);
 router.post('/login', validateLogin, new AuthController().login);
 router.get('/dashboard', jwtMiddleware, new DashboardController().greet);
-router.post('/email', new EmailController().email);
 
 export default router;
