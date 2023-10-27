25cd2ac1e1a7011878a7e7e846855eee4d856de4
Fernando Gurgel <gurgelff@gmail.com>
2023-10-27 00:11:35 -0300

    feat(controller): Home: Atribui a rota / como ponto de informações
    Por padrão, é uma boa prática mostrar o esquema das rotas na root
    
    Referencia o Swagger.json para se obter maiores informações

--- src/controller/HomeController.ts
+++ src/controller/HomeController.ts
@@ -5,27 +5,32 @@ export class HomeController {
    * @swagger
    * /:
    *   get:
-   *     summary: Hello
+   *     summary: Informações da API
    *     tags: [Home]
-   *     consumes:
-   *       - application/json
-   *     produces:
-   *       - application/json
    *     responses:
    *       '200':
-   *           description: 'requisição executada com sucesso'
-   *           content:
-   *             application/json:
-   *               schema:
-   *                 type: object
-   *                 properties:
-   *                   status:
-   *                     type: boolean
-   *                   data:
-   *                     type: object
-   *                     description: 'objeto json de retorno'
+   *         description: 'requisição executada com sucesso'
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: boolean
+   *                 data:
+   *                   type: object
+   *                   description: 'objeto json de retorno'
+   *                   properties:
+   *                     message:
+   *                       type: string
+   *                       example: "Acesse a documentação da API em /swagger"
    */
-  hello(_req: Request, res: Response) {
-    return res.status(200).send('Hello');
-  }
+  info = (_req: Request, res: Response) => {
+    return res.status(200).json({
+      status: true,
+      data: {
+        message: 'Acesse a documentação da API em /swagger.json'
+      }
+    });
+  };
 }
