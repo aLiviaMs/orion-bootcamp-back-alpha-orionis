f468f843f18cad7dfcb14c26692e995419289186
Fernando Gurgel <gurgelff@gmail.com>
2023-10-27 00:03:42 -0300

    feat(middleware): Busca o usuário pelo ID
    Faz o tratamento do ID internamente, assegurando que o tipo de dado confere

---
+++ src/middleware/searchID.ts
@@ -0,0 +1,48 @@
+import { Request, Response, NextFunction } from 'express';
+import { ObjectId, Repository } from 'typeorm';
+import { User } from '../entity/User';
+import { MongoDBDataSource } from '../config/database';
+import { ObjectId as convertToObjectID } from 'mongodb';
+
+/**
+ * Middleware responsável por buscar um usuário pelo ID.
+ * @param req Requisição contendo o ID do usuário.
+ * @param res No formato de JSON contendo a mensagem de erro
+ * @param next Executa a próxima função.
+ * @returns Mensagem de erro ou executa a próxima função.
+ */
+export const searchID = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> => {
+  const id: string = req.params.id ?? (req.body.id as string);
+
+  if (!id) {
+    return res.json({
+      status: false,
+      data: {
+        message: 'ID de usuário não informado.'
+      }
+    });
+  }
+
+  const userID: ObjectId = new convertToObjectID(id);
+
+  const UserRepository: Repository<User> =
+    MongoDBDataSource.getRepository(User);
+  const user: User = await UserRepository.findOne({ where: { _id: userID } });
+
+  if (!user?._id) {
+    return res.json({
+      status: false,
+      data: {
+        message: 'ID de usuário inválido.'
+      }
+    });
+  }
+
+  req.body.user ??= user;
+
+  next();
+};
