465548204775b174865b9e95c13fe1075a35a00d
Fernando Gurgel <gurgelff@gmail.com>
2023-10-27 00:06:45 -0300

    feat(middleware): Busca o usuário pelo e-mail
    Ao encontrar, insere o user no request body

---
+++ src/middleware/searchEmail.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from 'express';
+import { findOne } from '../utils/auth';
+import { User } from '../entity/User';
+
+export const searchEmail = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> => {
+  const email: string = req.body.email;
+
+  if (!email) {
+    return res.status(400).json({
+      status: false,
+      data: {
+        message: 'Email não informado.'
+      }
+    });
+  }
+
+  try {
+    const user: User = await findOne({ where: { email } });
+
+    if (!user?._id) {
+      return res.status(400).json({
+        status: false,
+        data: {
+          message: 'Não foi possível encontrar sua conta.'
+        }
+      });
+    }
+
+    req.body.user ??= user;
+  } catch (_err) {
+    return res.status(400).json({
+      status: false,
+      data: {
+        message: 'Não foi possível encontrar sua conta.'
+      }
+    });
+  }
+
+  next();
+};
