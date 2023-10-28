af4445790be8223c93312606e4c5b6895f917fa9
Fernando Gurgel <gurgelff@gmail.com>
2023-10-28 01:38:40 -0300

    docs(swagger/Auth): Conserta estrutura de dados da response 200

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -19,14 +19,11 @@ export class AuthController {
    *             properties:
    *               email:
    *                 type: string
-   *                 example: "email@domain.com"
    *               password:
    *                 type: string
-   *                 example: "Senh@Fort3"
    *               isRememberEnabled:
    *                 required: false
    *                 type: boolean
-   *                 example: true
    *                 description: Se o usuário deseja permanecer logado
    *     responses:
    *       '200':
@@ -38,10 +35,11 @@ export class AuthController {
    *               properties:
    *                 status:
    *                   type: boolean
+   *                   example: true
    *                 data:
    *                   type: object
    *                   description: Resposta JSON contendo o token de autenticação
-   *                   example: { status: true, data: { token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" } }
+   *                   example: { token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" }
    *       '400':
    *         description: Requisição inválida
    *         content:
@@ -51,10 +49,11 @@ export class AuthController {
    *               properties:
    *                 status:
    *                   type: boolean
+   *                   example: false
    *                 data:
    *                   type: object
    *                   description: Resposta JSON contendo a mensagem de erro
-   *                   example: { status: false, data: { message: "Não foi possível encontrar sua conta." } }
+   *                   example: { message: "Não foi possível encontrar sua conta." }
    */
 
   login = async (req: Request, res: Response): Promise<Response> => {
