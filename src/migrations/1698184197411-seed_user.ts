787eb6cebccb88ff95cf5f9d59d431a65e22c6e1
Fernando Gurgel <gurgelff@gmail.com>
2023-10-28 01:25:17 -0300

    feat(migrations): usa senha forte e email dedicado para test user

--- src/migrations/1698184197411-seed_user.ts
+++ src/migrations/1698184197411-seed_user.ts
@@ -3,13 +3,15 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class SeedUser1698184197411 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const user = queryRunner.manager.create('User', {
-      email: 'email@domain.com',
-      password: '$2b$12$un44BpeTTyJQKrhf9K3xpuCKtyjvGQx2Aogt2QNkf0LZPgg3M9wdm'
+      email: 'explorador-orion@proton.me',
+      password: '$2a$12$ANoifDVbjR4X2S0cVVPREO9heDFT43AAbv8g4dIXYl945VeWoOUty'
     });
     await queryRunner.manager.save('User', user);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.manager.delete('User', { email: 'email@domain.com' });
+    await queryRunner.manager.delete('User', {
+      email: 'explorador-orion@proton.me'
+    });
   }
 }
