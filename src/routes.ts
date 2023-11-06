43f693dfb112eadd6a10424e239626125698d2bc
Fernando Gurgel <gurgelff@gmail.com>
2023-11-06 00:44:54 -0300

    feat(routes): Rotas do Weather com Controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,47 @@
 import { Router } from 'express';
-import { HomeController } from './controller/HomeController';
+import { IndexController } from './controller/IndexController';
 import { AuthController } from './controller/AuthController';
 import { DashboardController } from './controller/DashboardController';
+import { ForgotPasswordController } from './controller/ForgotPasswordController';
+import { ResetPasswordController } from './controller/ResetPasswordController';
 import { jwtMiddleware } from './middleware/jwtMiddleware';
 import { validateLogin } from './middleware/validateLogin';
+import { validatePassword } from './middleware/validatePassword';
+import { searchEmail } from './middleware/searchEmail';
+import { searchID } from './middleware/searchID';
+import { verifyResetToken } from './middleware/verifyResetToken';
 import { validateEmail } from './middleware/validateEmail';
+import { WeatherController } from './controller/WeatherController';
+
 const router = Router();
 
-router.get('/', new HomeController().hello);
+router.get('/', new IndexController().info);
+
 router.post('/login', validateEmail, validateLogin, new AuthController().login);
+
 router.get('/dashboard', jwtMiddleware, new DashboardController().greet);
+router.get('/weather', jwtMiddleware, new WeatherController().getWeatherData);
+
+router.post(
+  '/forgot-password',
+  validateEmail,
+  searchEmail,
+  new ForgotPasswordController().forgotPassword
+);
+
+router.get(
+  '/reset-password/:id/:resetToken',
+  searchID,
+  verifyResetToken,
+  new ResetPasswordController().getResetToken
+);
+
+router.post(
+  '/reset-password',
+  validatePassword,
+  searchID,
+  verifyResetToken,
+  new ResetPasswordController().resetPassword
+);
 
 export default router;
