c22834613012ddca9e994f91dcff345f46bb3005
Hugo Tiburcio <hugotiburcio@outlook.com>
2023-12-06 12:33:53 -0300

    test: Add unit tests for sendEmail method

---
+++ src/tests/sendEmail.test.ts
@@ -0,0 +1,43 @@
+import { sendEmail } from '../utils/email';
+import nodemailer from 'nodemailer';
+
+jest.mock('nodemailer', () => ({
+  createTransport: jest.fn().mockReturnValue({
+    sendMail: jest.fn()
+  })
+}));
+
+const mockedSendMail = nodemailer.createTransport().sendMail as jest.Mock;
+
+it('deve enviar email', async () => {
+  mockedSendMail.mockResolvedValue({
+    accepted: ['teste@examplo.com']
+  });
+
+  const result: boolean = await sendEmail(
+    'endereço@exemplo.com',
+    'Assunto Teste',
+    '<p>Email de Teste</p>'
+  );
+
+  expect(result).toBe(true);
+  expect(mockedSendMail).toHaveBeenCalledWith(
+    expect.objectContaining({
+      to: 'endereço@exemplo.com',
+      subject: 'Assunto Teste',
+      html: '<p>Email de Teste</p>'
+    })
+  );
+});
+
+it('deve retornar erro ao enviar email', async () => {
+  mockedSendMail.mockRejectedValue(new Error('Erro ao enviar email'));
+
+  const result: boolean = await sendEmail(
+    'endereço@exemplo.com',
+    'Assunto Teste',
+    '<p>Email de Teste</p>'
+  );
+
+  expect(result).toBe(false);
+});
